c5df98a92de3cdcaad38a18e452fd69f4cb57f36
Dimitris Katsioulas <dimitris@33clouds.com>
2024-06-20 19:19:41 +0300

    Initial develop

--- resources/js/Pages/SideBar.vue
+++ resources/js/Pages/SideBar.vue
@@ -9,7 +9,6 @@
 
       <li><router-link to="/about">About</router-link></li>
       <li><router-link to="/contact">Contact</router-link></li>
-      <li><router-link to="/test">Test</router-link></li>
     </ul>
     <button class="sidebar-toggle-button" @click="$emit('toggle-sidebar')">
       <i :class="isOpen ? 'fas fa-arrow-left' : 'fas fa-arrow-right'"></i>
